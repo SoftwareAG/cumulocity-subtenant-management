f2db678116782347597987c40ffae7878fa1fbdb
Tristan Bastian <tristan.bastian@softwareag.com>
2021-06-10 10:07:28 +0200

    add some comments

--- src/services/custom-api.service.ts
+++ src/services/custom-api.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { ApiService, ApiCallOptions } from '@c8y/ngx-components/api';
 import { Client, FetchClient, IFetchOptions } from '@c8y/client';
 
+/**
+ * Customized version of the c8y ApiService to also show loading indicator for custom created c8y clients.
+ */
 @Injectable()
 export class CustomApiService extends ApiService {
   constructor(private client1: FetchClient) {
     super(client1);
   }
 
+  /**
+   * Pushes calls made by provided client parameter also into the calls observable, causing the loading indicator to also be shown for those calls.
+   */
   hookIntoCustomClientFetch(client: Client): void {
     const fetch = client.core.fetch.bind(client.core);
     client.core.fetch = async (url, options: ApiCallOptions & IFetchOptions = { method: 'GET' }) => {
