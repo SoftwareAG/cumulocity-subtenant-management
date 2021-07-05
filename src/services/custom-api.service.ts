ee8f2955d65f7374512d87984fd3064c0c49c15a
Tristan Bastian <tristan.bastian@softwareag.com>
2021-07-05 16:43:16 +0200

    remove not required constructor

--- src/services/custom-api.service.ts
+++ src/services/custom-api.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ApiService, ApiCallOptions } from '@c8y/ngx-components/api';
-import { Client, FetchClient, IFetchOptions } from '@c8y/client';
+import { Client, IFetchOptions } from '@c8y/client';
 
 /**
  * Customized version of the c8y ApiService to also show loading indicator for custom created c8y clients.
  */
 @Injectable()
 export class CustomApiService extends ApiService {
-  constructor(private client1: FetchClient) {
-    super(client1);
-  }
-
   /**
    * Pushes calls made by provided client parameter also into the calls observable, causing the loading indicator to also be shown for those calls.
    */
