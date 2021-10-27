e133e7ac4f0a39fb6a2010a86fa34d4e2645df75
Tristan Bastian <tristan.bastian@softwareag.com>
2021-10-27 19:25:09 +0200

    refactor tenant app logs functionality (#29)

--- src/modules/tenant-app-logs/apps-of-tenant-tab.factory.ts
+++ src/modules/tenant-app-logs/apps-of-tenant-tab.factory.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IApplication } from '@c8y/client';
 import { Tab, TabFactory } from '@c8y/ngx-components';
+import { IApplicationManagedObject } from '@models/application-mo';
 import { merge, Observable } from 'rxjs';
 import { filter, map, take, timeout } from 'rxjs/operators';
 import { AppsOfTenantService } from './apps-of-tenant.service';
@@ -33,23 +33,24 @@ export class AppsOfTenantTabFactory implements TabFactory {
     if (!tenantId) {
       return [];
     }
-    const apps = await this.appsOfTenant.getCachedAppsOfTenant(tenantId);
+    const apps = await this.appsOfTenant.getCachedAppsSupportingLogs(tenantId);
     const tabs = this.mapAppsToTabs(apps, tenantId);
     return tabs;
   }
 
-  private mapAppsToTabs(apps: IApplication[], tenantId?: string): Tab[] {
-    return apps
-      .filter(
-        (app: IApplication & { manifest: { isolation: string } }) =>
-          app.type === 'MICROSERVICE' && (app.owner.tenant.id === tenantId || app.manifest.isolation === 'PER_TENANT')
-      )
-      .map((app) => {
-        return {
-          label: `Logs: ${app.name || app.contextPath || 'unknown'}`,
-          path: `tenants/${tenantId}/app-log/${app.id}`,
-          icon: 'file-text'
-        } as Tab;
-      });
+  private mapAppsToTabs(apps: IApplicationManagedObject[], tenantId: string): Tab[] {
+    const tabs = new Array<Tab>();
+    apps.forEach((app) => {
+      if (app.c8y_Status && app.c8y_Status.instances) {
+        Object.keys(app.c8y_Status.instances).forEach((instance, index) => {
+          tabs.push({
+            label: `Logs: ${app.name || 'unknown'} #${index + 1}`,
+            path: `tenants/${tenantId}/app-log/${app.applicationId}/${instance}`,
+            icon: 'file-text'
+          } as Tab);
+        });
+      }
+    });
+    return tabs;
   }
 }
